80923eaff0c1d2022d44eef8a6963f34218e8013
Piotr Zaniewski <piotr@upbound.io>
2023-11-06 18:04:18 +0100

    Confirm input

--- fn.go
+++ fn.go
@@ -25,9 +25,16 @@ type Function struct {
 func (f *Function) RunFunction(_ context.Context, req *fnv1beta1.RunFunctionRequest) (*fnv1beta1.RunFunctionResponse, error) {
 	f.log.Info("Running function", "tag", req.GetMeta().GetTag())
 
+	// Create a response to the request. This copies the desired state and
+	// pipeline context from the request to the response.
 	rsp := response.To(req, response.DefaultTTL)
 
+	// This input comes from the label folder in
+	// "github.com/Piotr1215/function-simple-label/label/v1beta1"
 	in := &v1beta1.Input{}
+
+	// Confirm we are getting input from the request
+	f.log.Debug("Getting input", "input", in)
 	if err := request.GetInput(req, in); err != nil {
 		response.Fatal(rsp, errors.Wrapf(err, "cannot get Function input from %T", req))
 		return rsp, nil
