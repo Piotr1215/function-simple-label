550f208be1a953629ad79fc365a55086d1d264f1
Piotr Zaniewski <piotr@upbound.io>
2023-11-06 16:17:04 +0100

    Ready function

--- fn.go
+++ fn.go
@@ -5,12 +5,13 @@ import (
 
 	"github.com/crossplane/crossplane-runtime/pkg/errors"
 	"github.com/crossplane/crossplane-runtime/pkg/logging"
+	"github.com/crossplane/crossplane-runtime/pkg/meta"
 
 	fnv1beta1 "github.com/crossplane/function-sdk-go/proto/v1beta1"
 	"github.com/crossplane/function-sdk-go/request"
 	"github.com/crossplane/function-sdk-go/response"
 
-	"github.com/Piotr1215/function-simple-label/input/v1beta1"
+	"github.com/Piotr1215/function-simple-label/label/v1beta1"
 )
 
 // Function returns whatever response you ask it to.
@@ -32,10 +33,27 @@ func (f *Function) RunFunction(_ context.Context, req *fnv1beta1.RunFunctionRequ
 		response.Fatal(rsp, errors.Wrapf(err, "cannot get Function input from %T", req))
 		return rsp, nil
 	}
+	desired, err := request.GetDesiredComposedResources(req)
+	if err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot get desired composed resources from %T", req))
+		return rsp, nil
+	}
+	f.log.Debug("Found desired resources", "count", len(desired))
+	// This will became the value of label "crossplane.io/test.label"
+	// If the lable is missing it will be added with the value of label field
+	// If the label is present its value will be updated
+	for _, dr := range desired {
+		if _, ok := dr.Resource.GetLabels()["crossplane.io/test-label"]; ok {
+			continue
+		}
 
-	// TODO: Add your Function logic here!
+		meta.AddLabels(dr.Resource, map[string]string{"crossplane.io/test-label": in.Label})
+	}
 	response.Normalf(rsp, "I was run with input %q!", in.Label)
-	f.log.Info("I was run!", "input", in.Example)
-
+	f.log.Info("I was run!", "input", in.Label)
+	if err := response.SetDesiredComposedResources(rsp, desired); err != nil {
+		response.Fatal(rsp, errors.Wrapf(err, "cannot set desired composed resources from %T", req))
+		return rsp, nil
+	}
 	return rsp, nil
 }
